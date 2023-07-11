c8d4be186699b4642755a312c4a92f75ce73bee8
Santi <gussxsat@gmail.com>
2023-07-10 19:02:34 -0500

    Creé un hook personalizado

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,48 +1,11 @@
-import React, { useState, useEffect } from 'react';
 import TaskForm from './TaskForm';
 import Task from './Task';
 import '../hojas-de-estilo/TaskList.css';
+import { useOptimize } from '../hooks/useOptimize';
 
 function TaskList() {
 
-  const [tareas, setTareas] = useState([]);
-
-  const agregarTarea = tarea => {
-    if (tarea.texto.trim()) {
-      tarea.texto = tarea.texto.trim();
-      const tareasActualizadas = [tarea, ...tareas];
-      setTareas(tareasActualizadas);
-    }
-  }
-
-  const eliminarTarea = id => {
-    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
-    setTareas(tareasActualizadas);
-  }
-
-  const completarTarea = id => {
-    const tareasActualizadas = tareas.map(tarea => {
-      if (tarea.id === id) {
-        tarea.completada = !tarea.completada;
-      }
-      return tarea;
-    });
-    setTareas(tareasActualizadas);
-
-    localStorage.setItem("tareas", JSON.stringify(tareasActualizadas))
-  };
-  
-  useEffect(() => {
-    const localStorageData = localStorage.getItem("tareas");
-    if(localStorageData){
-      try{
-        const storedTareas = JSON.parse(localStorageData);
-        setTareas(storedTareas);
-      }catch(err){
-        console.err("Error parsing favourite items from localStorage")
-      }
-    }
-  }, []);
+  const {agregarTarea, eliminarTarea, completarTarea, tareas} = useOptimize()
   
   return (
     <>
